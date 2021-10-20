aaeaa6c19e7e34a05af18885044d3adcc60f8ffe
Harry <hrgus3@gmail.com>
2021-10-20 17:45:53 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+let multiply; 
+let num1 = 2;
+let num2 = 31;
+
+multiply = (num1 * num2);
+
+let random;
+
+random = Math.round(Math.random() * 10);
+
+let mod;
+let num3 = 12;
+let num4 = 3;
+
+mod = (num3 / num4);
+
+let max;
+
+max = Math.max(1,12,15,20);
